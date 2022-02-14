ee1ad10f66808a2a091b6da7d2f4210e464cc029
117marta <117marta@gmail.com>
2022-02-14 15:26:58 +0100

    Create Photo model

--- photo/photo_app/models.py
+++ photo/photo_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class PhotoModel(models.Model):
+    photo = models.ImageField(upload_to='img/', blank=True, verbose_name='Choose photo')
+    description = models.TextField(verbose_name='Photo comment')
+    name = models.CharField(max_length=50, null=True)
+    latitude = models.DecimalField(max_digits=8, decimal_places=6, null=True)
+    longitude = models.DecimalField(max_digits=8, decimal_places=6, null=True)
+    altitude = models.DecimalField(max_digits=8, decimal_places=6, null=True)
+    date_taken = models.DateTimeField(null=True)
+    width = models.SmallIntegerField(null=True)
+    height = models.SmallIntegerField(null=True)
+    camera_name = models.CharField(max_length=100, null=True)
+    camera_model = models.CharField(max_length=100, null=True)

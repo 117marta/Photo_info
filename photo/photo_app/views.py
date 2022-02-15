b20efcd54d51a0d4464fb1104231a85d79ae1bd7
117marta <117marta@gmail.com>
2022-02-15 09:47:45 +0100

    PhotoInfoView - get exif info from photo

--- photo/photo_app/views.py
+++ photo/photo_app/views.py
@@ -1,9 +1,11 @@
+import folium as folium
 from django.shortcuts import render, HttpResponse
 from .models import PhotoModel
 from django.views import View
 from django.views.generic.edit import CreateView
 from django.contrib.messages.views import SuccessMessageMixin
 from PIL import Image
+import piexif
 
 
 class PhotoUploadView(SuccessMessageMixin, CreateView):
@@ -26,9 +28,34 @@ class PhotoInfoView(View):
 
         if img.photo:
             img_opened = Image.open(img.photo.path)
+            try:
+                exif_dict = piexif.load(img_opened.info['exif'])
+                lat = exif_dict['GPS'][piexif.GPSIFD.GPSLatitude]
+                lat_ref = exif_dict['GPS'][piexif.GPSIFD.GPSLatitudeRef]
+                lon = exif_dict['GPS'][piexif.GPSIFD.GPSLongitude]
+                lon_ref = exif_dict['GPS'][piexif.GPSIFD.GPSLongitudeRef]
+                alt = exif_dict['GPS'][piexif.GPSIFD.GPSAltitude]
+                height = exif_dict["0th"][257]
+                width = exif_dict["0th"][256]
+                date = exif_dict['0th'][piexif.ImageIFD.DateTime]
+                camera = exif_dict["0th"][271]
+                model = exif_dict["0th"][272]
+            except KeyError:
+                return HttpResponse('No exif data in photo.')
+
             ctx = {
                 'img_opened': img_opened,
                 'img': img,
+                'lat': lat,
+                'lat_ref': lat_ref,
+                'lon': lon,
+                'lon_ref': lon_ref,
+                'alt': alt,
+                'width': width,
+                'height': height,
+                'date': date,
+                'camera': camera,
+                'model': model,
             }
             return render(request=request, template_name='photo_app/photo-info.html', context=ctx)
         else:
